1a0888ece0497cdeea5a22decfcd8265f9f63ccd
gbarroso84 <gbarroso@bionexo.com>
2020-01-02 16:08:45 -0300

    intro

---
+++ my-project/src/components/Diretivas.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="diretivas">
+      <!-- Ao trabalhar com diretivas, eu especifico o que eu quero dentro de data e posso
+      ignorar as chaves, não preciso mais deles -->
+      <img
+        :src="srcImg"
+        v-bind:title="titleImg"
+    >
+    <!-- Quando uso o : direto, o Vue já entende que eu quero usar o v-bind:algumacoisa -->
+      <p><strong>X-men</strong></p>
+      <p v-text="'texto qualquer'"/>
+      <p v-text='outroTexto'/>
+      <br />
+      <br />
+      <!-- v-model -->
+      <input type="text" v-model='textoDigitado'>
+      <br />
+      {{textoDigitado}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Diretivas',
+  data () {
+    return {
+      srcImg: '../assets/logo.png',
+      titleImg: 'X-men Group',
+      outroTexto: 'Outro texto',
+      textoDigitado: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+p {
+    color: green;
+}
+</style>
