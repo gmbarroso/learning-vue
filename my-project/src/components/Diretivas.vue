0a12e32f7419379c44b69e9ad0c6761c0ba97a53
gmbarroso <barroso.guilherme@gmail.com>
2020-01-03 10:45:35 -0300

    add v-if

--- my-project/src/components/Diretivas.vue
+++ my-project/src/components/Diretivas.vue
@@ -2,11 +2,11 @@
   <div class="diretivas">
       <!-- Ao trabalhar com diretivas, eu especifico o que eu quero dentro de data e posso
       ignorar as chaves, não preciso mais deles -->
-      <img
-        :src="srcImg"
-        v-bind:title="titleImg"
-    >
+      <!-- <img v-bind:src="srcImg" v-bind:title="titleImg" /> -->
     <!-- Quando uso o : direto, o Vue já entende que eu quero usar o v-bind:algumacoisa -->
+      <div v-if="textoDigitado == 1" class='quadrado'></div>
+      <div v-else-if="textoDigitado == 2" class='quadrado2'></div>
+      <div v-else class='quadrado3'></div>
       <p><strong>X-men</strong></p>
       <p v-text="'texto qualquer'"/>
       <p v-text='outroTexto'/>
@@ -24,10 +24,10 @@ export default {
   name: 'Diretivas',
   data () {
     return {
-      srcImg: '../assets/logo.png',
+      srcImg: "../assets/xmenGroup.jpg",
       titleImg: 'X-men Group',
       outroTexto: 'Outro texto',
-      // textoDigitado: ''
+      textoDigitado: ''
     }
   },
   methods: {
@@ -39,7 +39,31 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+.diretivas {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.quadrado {
+  width: 100px;
+  height: 100px;
+  background-color:green;
+}
+.quadrado2 {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+.quadrado3 {
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+}
 p {
     color: green;
+}
+input {
+  width: 200px;
 }
 </style>
